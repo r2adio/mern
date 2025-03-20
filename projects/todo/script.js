6e223bbfb4004c34a7632aaebdfbbd142d62a6c5
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-03-20 18:40:46 +0530

    completed a basic todo app.

--- projects/todo/script.js
+++ projects/todo/script.js
@@ -1,18 +1,46 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+//first and for most the DOM content must be loaded first
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-let tasks = [];
-addTaskButton.addEventListener("click", () => {
-  const taskTest = todoInput.value.trim();
-  if (taskTest === "") return;
-  const newTask = { id: Date.now(), text: taskTest, completed: false };
-  tasks.push(newTask);
-  saveTasks();
-  todoInput.value = ""; //clears the input box
-  console.log(tasks);
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => renderTask(task));
+
+  addTaskButton.addEventListener("click", () => {
+    const taskTest = todoInput.value.trim();
+    if (taskTest === "") return;
+    const newTask = { id: Date.now(), text: taskTest, completed: false };
+    tasks.push(newTask);
+    saveTasks();
+    renderTask();
+    todoInput.value = ""; //clears the input box
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+
+    li.innerHTML = `<span>${task.text}</span> <button>delete</button>`;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //prevent toggle from firing
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+    todoList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
-
-function saveTasks() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
