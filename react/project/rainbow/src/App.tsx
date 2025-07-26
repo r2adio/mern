c09ef7e9de406b55ee3de6522439b4e7fdce8e27
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-26 18:21:42 +0530

    react project, rainbow: add some other colors

--- react/project/rainbow/src/App.tsx
+++ react/project/rainbow/src/App.tsx
@@ -4,39 +4,51 @@ import "./App.css";
 function App() {
   const [color, setColor] = useState("olive"); // define the default state of application
   return (
-    <>
-      <div
-        className="w-full h-screen duration-200"
-        // className="fixed flex flex-wra justify-center h-40 w-40 top-2 left-1/3"
-        style={{ backgroundColor: color }} // way to write custom inline css
-      >
-        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-black px-3 py-2 rounded-3xl">
-            <button
-              onClick={() => setColor("red")}
-              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{ backgroundColor: "red" }}
-            >
-              RED
-            </button>
-            <button
-              onClick={() => setColor("blue")}
-              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{ backgroundColor: "blue" }}
-            >
-              YELLOW
-            </button>
-            <button
-              onClick={() => setColor("green")}
-              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-              style={{ backgroundColor: "green" }}
-            >
-              GREEN
-            </button>
-          </div>
+    <div
+      className="w-full h-screen duration-200"
+      // className="fixed flex flex-wra justify-center h-40 w-40 top-2 left-1/3"
+      style={{ backgroundColor: color }} // way to write custom inline css
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-black px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            RED
+          </button>
+          <button
+            onClick={() => setColor("white")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "white" }}
+          >
+            WHITE
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "orange" }}
+          >
+            ORANGE
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            BLUE
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            GREEN
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
