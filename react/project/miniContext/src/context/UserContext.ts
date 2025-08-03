46979501b71935ae80aba37908e27cb555af5307
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-03 23:39:41 +0530

    react-project: context api project

---
+++ react/project/miniContext/src/context/UserContext.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+// Context gives one major thing -> `provider`
+// every context is a provider, ??
+const UserContext = React.createContext();
+
+export default UserContext;
